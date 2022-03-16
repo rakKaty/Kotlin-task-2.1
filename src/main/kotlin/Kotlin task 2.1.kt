f81ebd4b2d22be7eab457beb61115d058afc66e2
Ekaterina Rak <rak.katia@gmail.com>
2022-03-16 20:18:50 +0100

    Second Edition

--- src/main/kotlin/Kotlin task 2.1.kt
+++ src/main/kotlin/Kotlin task 2.1.kt
@@ -1,13 +1,11 @@
+import java.lang.Integer.max
 
 fun main() {
-    var percentCommission = 0.0075
-    var amount = 135_000_00
+    val percentCommission = 0.0075
+    val amount = 135_000_00
 
 
-    if(amount < 35_00) {
-        println("Сумма перевода должна составлять минимум 35руб.")
-    } else {
-        var commissionAmount: Double = amount * percentCommission
+        val commissionAmount  = max ((amount * percentCommission).toInt(), 35_00)
         println("Коммисия в копейках: $commissionAmount")
-    }
+
 }
